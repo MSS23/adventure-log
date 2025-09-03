049c73b652ffab914fd0b9f7b2b5d02ad74ec761
ManrajS23 <msidhu861@gmail.com>
2025-09-03 10:08:11 +0100

    Test commit with more lint errors

--- test-lint-error.ts
+++ test-lint-error.ts
@@ -1,10 +1,13 @@
 // This file is intentionally created with lint errors to test pre-commit hooks
+// Adding more lint errors to test the pre-commit hook
 
 const unused_variable = "this should trigger an ESLint error";
+const another_unused_var = "more unused variables";
 
 function badlyFormatted(){
-  console.log( "bad formatting" )
-  const another_unused="more issues"
+  console.log( "bad formatting" );
+  const another_unused="more issues";
+  console.log("missing semicolon")
 }
 
-export    {   badlyFormatted    }
+export    {   badlyFormatted    };
